e90e7a6db5f583eafcb6899cf526bcf91dd90023
Akansha Roy <akansharoy2008@gmail.com>
2025-11-04 23:09:21 +0530

    Initial commit

---
+++ src/main/java/com/cryptex/cryptex_authService/CryptexAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cryptex.cryptex_authService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptexAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptexAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptex/cryptex_authService/CryptexAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptex.cryptex_authService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptexAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
